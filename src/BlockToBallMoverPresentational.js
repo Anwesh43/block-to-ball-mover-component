1b836b168e2b4fddc5bfbf5b78e3bc76e0f1c314
Anwesh43 <anweshthecool0@gmail.com>
2020-05-05 00:42:26 +0530

    using BlockToBallMover component

--- src/BlockToBallMoverPresentational.js
+++ src/BlockToBallMoverPresentational.js
@@ -1,5 +1,6 @@
 import {useStyle} from './hooks'
 import React from 'react'
+import BlockToBallMoverPresentational from './BlockToBallMoverPresentational'
 
 const BlockToBallMoverPresentational = ({w, h, scale, onClick}) => {
     const style = useStyle(w, h, scale)
